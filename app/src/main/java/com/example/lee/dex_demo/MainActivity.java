43984a094a69cf95f264fbf57305adbeca75f1c0
xtl1889 <764917136@qq.com>
2016-12-07 10:44:07 +0800

    test commit 2

--- app/src/main/java/com/example/lee/dex_demo/MainActivity.java
+++ app/src/main/java/com/example/lee/dex_demo/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //测试提交
+        //测试提交2
         initView();
     }
 
